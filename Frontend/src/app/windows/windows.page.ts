01d8354e32e7f2d4c691931f134e5b75cd4cf311
Morgan Chételat <morganch@pop-os.localdomain>
2019-05-11 14:41:49 +0200

    Add. windows page

---
+++ Frontend/src/app/windows/windows.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-windows',
+  templateUrl: './windows.page.html',
+  styleUrls: ['./windows.page.scss'],
+})
+export class WindowsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
