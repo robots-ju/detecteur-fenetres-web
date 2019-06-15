1dcc3069c45b12a53f8d012d2b525f02bb15bceb
Morgan Chételat <morgan.chetelat@gmail.com>
2019-06-15 10:11:24 +0200

    Router + Bootstrap

--- app-web/src/router.js
+++ app-web/src/router.js
@@ -1,12 +1,17 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
+import Auth from './views/Auth';
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
     mode: 'history',
     routes: [
-
+        {
+            path: '/auth',
+            component: Auth
+        }
     ]
 })
 
