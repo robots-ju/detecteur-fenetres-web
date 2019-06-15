d56e94aa42a8480ba0d6365efa2fb4e1806f6e72
Morgan Chételat <morgan.chetelat@gmail.com>
2019-06-15 10:10:16 +0200

    Création de la page d'authentification

---
+++ app-web/src/views/Auth.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container d-flex flex-column">
+    <div class="card my-5 w-100">
+      <div class="card-header">Connection</div>
+      <div class="card-body align-items-center justify-content-between d-flex flex-column">
+        <form @submit.prevent="trySubmit">
+          <div class="form-group">
+            <label>Email:</label>
+            <input v-model="form.email" type="text" placeholder="Email" class="form-control">
+          </div>
+          <div class="form-group">
+            <label>Password:</label>
+            <input
+              v-model="form.password"
+              type="password"
+              placeholder="Password"
+              class="form-control"
+            >
+          </div>
+          <button
+            class="btn btn-success"
+          >Se connecter</button>
+        </form>
+      </div>
+    </div>
+    <div v-if="success" class="alert alert-success mt-2 w-100">Connection réussie !</div>
+    <div v-if="errors.length" class="alert alert-danger w-100">
+      <li class="list-group pt-1">
+        <ul v-for="error in errors" :key="error">- {{ error }}</ul>
+      </li>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: "",
+        password: ""
+      },
+      success: false,
+      errors: []
+    };
+  },
+  methods: {
+    trySubmit() {
+      this.errors = [];
+      if (this.form.password === "1234") {
+        this.success = true;
+      } else {
+        this.errors.push("Mauvais mot de passe");
+        this.success = false;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
